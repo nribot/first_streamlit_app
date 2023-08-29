4b3382b949011437f4e5ac813a9e08b6bf71289c
Núria Ribot Codina <43704144+nribot@users.noreply.github.com>
2023-08-30 01:11:01 +0200

    Update streamlit_app.py
    added 3 breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast')
-streamlit.text("Chocolate Milk and Cookies")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
