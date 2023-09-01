16b5872e548e1993d8d25b35a7349453b160cf9d
Núria Ribot Codina <43704144+nribot@users.noreply.github.com>
2023-09-01 18:11:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+# display text
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -8,27 +8,45 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# import fruit table
+
+# DISPLAY FRUIT TABLE PICKER
+
+# import fruit table data
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # set index by fruit name
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# display fruit pick list here so they can pick the fruit they want to include (default fruits)
+# display fruit pick list first so customers can pick the fruit they want to include (default fruits)
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
+# only show selected fruits
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display fruit table
 streamlit.dataframe(fruits_to_show)
 
+
+# DISPLAY FRUITYVICE API RESPONSE
+
 # we import a python package that lets us display the fruityvice api response
 import requests
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+# get the data
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+# write text to the screen
 streamlit.text(fruityvice_response.json())
+
+
+# normalize the json to a table / df (normalize)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# displays the table to the screen
+streamlit.dataframe(fruityvice_normalized)
 
 
 
